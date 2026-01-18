9c9a62eb3656eb320d1d51e30b80ff13683d70cb
BigNature <191410593+777Tu@users.noreply.github.com>
2026-01-18 18:45:03 +0100

    Refactor UrlPBPaths function and add main check

--- libs/urlpaths.py
+++ libs/urlpaths.py
@@ -1,9 +1,8 @@
-from urllib.parse import urlparse 
+from urllib.parse import urlparse
 
 def UrlPBPaths(Urlpaths):
             Paths = urlparse(Urlpaths)
             PBPaths: list=[]
-       
             parseMtds = Paths.scheme
             if Paths.scheme and Paths.netloc:
                 if Paths.netloc:
@@ -19,10 +18,14 @@ def UrlPBPaths(Urlpaths):
                     parseMtds +="?"+Paths.query
                     PBPaths.append(parseMtds)
                 if Paths.fragment:
-                    parseMtds +="#"+ Paths.fragment
+                    parseMtds +="#"+Paths.fragment
                     PBPaths.append(parseMtds)
             else:
                  PBPaths =[Urlpaths]
             return PBPaths
-for i in UrlPBPaths("https://user:pass@example.com/path?query#frag"):
-    print(i)            
+
+if __name__ == "__main__":
+                      url = "https://user:pass@example.com/path?query#frag"
+                      for paths in UrlPBPaths("https://user:pass@example.com/path?query#frag"):
+                          print(paths)
+                      
